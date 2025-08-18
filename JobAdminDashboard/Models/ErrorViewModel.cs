0ccf5fc3873e30c4998426dd1cf709aa2a1bf4d5
Ablazed-flame <harshitvyas2004@gmail.com>
2025-08-18 18:45:32 +0530

    Delete JobAdminDashboard directory

--- JobAdminDashboard/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JobAdminDashboard.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
